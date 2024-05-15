77ce602d207b2454fdb4b9dabd06e8030f548f32
Judy Tsai <judy.tsai@vpon.com>
2024-05-15 15:23:27 +0800

    Write README.md and LICENSE files
    To-do: Change plugin name & replace wiki url
    Modify some annotation in dart files.

--- vpon_plugin_poc/lib/interface/insterstitial_ad.dart
+++ vpon_plugin_poc/lib/interface/insterstitial_ad.dart
@@ -5,25 +5,19 @@ import 'ad_listeners.dart';
 import 'ad_request.dart';
 import 'ad_instance_manager.dart';
 
-/// A full-screen interstitial ad for the Vpon Plugin.
 class InterstitialAd extends AdWithoutView {
-  /// Creates an [InterstitialAd].
   InterstitialAd._({
     required String licenseKey,
     required this.request,
     required this.adLoadCallback,
   }) : super(licenseKey: licenseKey);
 
-  /// Targeting information used to fetch an [Ad].
   final VponAdRequest request;
 
-  /// Callback to be invoked when the ad finishes loading.
   final InterstitialAdLoadCallback adLoadCallback;
 
-  /// Callbacks to be invoked when ads show and dismiss full screen content.
   FullScreenContentCallback<InterstitialAd>? fullScreenContentCallback;
 
-  /// Loads an [InterstitialAd] with the given [licenseKey] and [request].
   static Future<void> load({
     required String licenseKey,
     required VponAdRequest request,
@@ -36,11 +30,7 @@ class InterstitialAd extends AdWithoutView {
 
     await instanceManager.loadInterstitialAd(ad);
   }
-
-  /// Displays this on top of the application.
-  ///
-  /// Set [fullScreenContentCallback] before calling this method to be
-  /// notified of events that occur when showing the ad.
+  
   Future<void> show() {
     return instanceManager.showAdWithoutView(this);
   }
